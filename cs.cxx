16acc7ee29709ef4de6ae1668ceb08a5ea33a5c2
Dimitar Pashov <d.pashov@gmail.com>
2016-01-02 00:12:07 +0000

    move functionality away from the C calbacks.

--- cs.cxx
+++ cs.cxx
@@ -25,6 +25,25 @@
 
 #include <systemd/sd-bus.h>
 
+// these are non-const in systemd/sd-bus.h but shall really be const!
+int sd_bus_creds_get_euid(const sd_bus_creds *c, uid_t *euid);
+int sd_bus_creds_get_egid(const sd_bus_creds *c, gid_t *egid);
+int sd_bus_creds_get_supplementary_gids(const sd_bus_creds *c, const gid_t **gids);
+
+
+int sd_bus_creds_get_euid(const sd_bus_creds *c, uid_t *euid) {
+    return sd_bus_creds_get_euid(const_cast<sd_bus_creds*>(c), euid);
+}
+int sd_bus_creds_get_egid(const sd_bus_creds *c, gid_t *egid) {
+    return sd_bus_creds_get_egid(const_cast<sd_bus_creds*>(c), egid);
+}
+int sd_bus_creds_get_supplementary_gids(const sd_bus_creds *c, const gid_t **gids) {
+    return sd_bus_creds_get_supplementary_gids(const_cast<sd_bus_creds*>(c), gids);
+}
+
+
+
+
 
 using std::vector;
 using std::string;
@@ -38,12 +57,15 @@ using std::setw;
 extern "C" const sd_bus_vtable job_ops_vtable[];
 
 
+using jid_t = uint64_t;
+
+
 class user_creds_t {
 public:
-    user_creds_t(uid_t uid, gid_t gid, int ngids, gid_t *gids): 
+    user_creds_t(const uid_t uid, const gid_t gid, const int ngids, const gid_t *gids): 
         uid(uid), gid(gid), gids(gids, gids+ngids) {}
     
-    user_creds_t(sd_bus_creds *creds):
+    user_creds_t(const sd_bus_creds *creds):
         uid(99), gid(99) {
         sd_bus_creds_get_euid(creds, &uid);
         sd_bus_creds_get_egid(creds, &gid);
@@ -66,15 +88,15 @@ ostream& operator<< (ostream &o, const user_creds_t &uc);
 
 class job_t {
 public:
-    uint64_t id;
+    jid_t id;
     const std::valarray<string> status_s {"queued", "running", "past"};
     enum status_t {queued, running, past};
     user_creds_t creds; 
     
-    job_t(string &name, string &req, user_creds_t &creds): 
-        name(name), req(req), creds(creds), id(++id0), status(queued) {}
-    job_t(const char *name, const char *req, sd_bus_creds *creds): 
-        name(string(name)), req(string(req)), creds(user_creds_t(creds)), id(++id0), status(queued) {}
+    job_t(const string &name, const string &req, const user_creds_t &creds, const jid_t id = 0): 
+        name(name), req(req), creds(creds), id(id), status(queued) {}
+    job_t(const char *name, const char *req, const sd_bus_creds *creds, const jid_t id = 0): 
+        name(string(name)), req(string(req)), creds(user_creds_t(creds)), id(id), status(queued) {}
     
     ~job_t() {
         std::cerr << "deleted: " << *this << '\n';
@@ -84,14 +106,11 @@ public:
     friend ostream& operator<< (ostream &, const job_t &);
     
 private:
-    static uint64_t id0;
     string name;
     string req;
     vector<string> nodes; // list of addresses
     status_t status;
 };
-
-uint64_t job_t::id0 = 0;
 
 
 
@@ -150,81 +169,49 @@ ostream& operator<< (ostream &o, const job_t &j) {
     return o;
 }
 
+// std::ostream& operator<< (std::ostream & o, const jobs_t& js) {
+//     for (auto& i : js) o << i.second << '\n';
+//     return o;
+// }
 
 
-using jid_t = uint64_t;
-using jobs_t = map<jid_t,job_t>;
-using jdel_lst_t = map<jid_t,string>;
+using jdel_lst_t = std::unordered_map<jid_t,string>;
 
-std::ostream& operator<< (std::ostream & o, const jobs_t& js) {
-    for (auto& i : js) o << i.second << '\n';
-    return o;
-}
 
 std::ostream& operator<< (std::ostream & o, const jdel_lst_t& js) {
     for (auto& i : js) o << i.first << ": " << i.second << '\n';
     return o;
 }
 
-
-
-
-extern "C" int jadd(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
-//     most of this is here rather than in the constructors of job_t etc.. because it is easy to return the proper errors to the bus.
-        sd_bus_creds *creds = sd_bus_message_get_creds(m);
-        uint64_t creds_mask = sd_bus_creds_get_mask(creds);
-        if (!(
-            (creds_mask & SD_BUS_CREDS_EUID  ) &&
-            (creds_mask & SD_BUS_CREDS_EGID  ) &&
-            (creds_mask & SD_BUS_CREDS_SUPPLEMENTARY_GIDS)         
-        )) {
-            fprintf(stderr, "uid, gid or supplementary gids of caller not available\n");
-            return -ENODATA;
-        }
-        
-        const char *name, *req;
-        int r = sd_bus_message_read(m, "as", 2, &name, &req);
-        if (r < 0) {
-                fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
-                return r;
-        }
-        
-//         job_t j(string(name), string(req), user_creds_t(creds)); //this gets into some mess
+class jobs_t {
+    jid_t id0;
+    map<jid_t,job_t> js;
+    
+public:    
+    jobs_t(const jid_t id0 = 0): id0(id0) {}
+    
+    jid_t add(const job_t &j) {
+        jid_t jid(++id0);
+        auto jn = js.emplace(jid, j); //try_emplace not in gcc yet (v5.3.0)
+        if (jn.second) jn.first->second.id = jid;
+        return jid;
+    }
+    
+    jid_t add(const char *name, const char *req, const sd_bus_creds *creds) {
         job_t j(name, req, creds);
-        
-        
-        jobs_t& jobs = *(static_cast<jobs_t*>(userdata));
-        jobs.emplace(j.id, j);  //try_emplace not in gcc yet (v5.3.0)
-        
-        return sd_bus_reply_method_return(m, "t", j.id);
-}
-
-extern "C" int jdel(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
-        sd_bus_creds *creds = sd_bus_message_get_creds(m);
-        uid_t uid(99); sd_bus_creds_get_euid(creds, &uid);
-        
-        jid_t *ids;
-        size_t sz;
-        
-//      the signature is really "at"
-        int r = sd_bus_message_read_array(m, 't', (const void**) &ids, &sz);
-        if (r < 0) {
-            fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
-            return r;
-        }
-        
-        jobs_t& jobs = *(static_cast<jobs_t*>(userdata));
+        return add(j);
+    }
+    
+    string del(const vector<jid_t>& jids, const user_creds_t &creds) {
         
         jdel_lst_t res;
-        for (jid_t *id = ids; id < ids+sz/sizeof(jid_t*); ++id) {
-            res.emplace(*id, "pending"); 
-        }
+        for (auto& jid: jids) res.emplace(jid, "pending");
    
         for (auto& jp: res) {
-            auto itr = jobs.find(jp.first);
-            if (itr != jobs.end()) {
-                if (itr->second.creds.uid == uid || uid == 0) {
-                    jobs.erase(itr);
+            auto itr = js.find(jp.first);
+            if (itr != js.end()) {
+                if (itr->second.creds.uid == creds.uid || 0 == creds.uid) {
+                    js.erase(itr);
                     jp.second = "done";
                 } else {
                     jp.second = "access denied";
@@ -236,17 +223,75 @@ extern "C" int jdel(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) 
         
         std::ostringstream s;
         s << std::left << '\n' << res;
-        return sd_bus_reply_method_return(m, "s", s.str().c_str());
+        return s.str();
+    }
+    
+    string del(const int njids, const jid_t *jids, const sd_bus_creds *creds) {
+        return del(vector<jid_t>(jids, jids+njids), user_creds_t(creds));
+    }
+    
+    string lst() {
+        std::ostringstream s;
+        s << std::left << '\n' << js;
+        return s.str();
+    }
+    
+
+};
+
+
+
+
+
+extern "C" int jadd(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
+//     most of this is here rather than in the constructors of job_t etc.. because it is easy to return the proper errors to the bus.
+    auto* creds = sd_bus_message_get_creds(m);
+    auto creds_mask = sd_bus_creds_get_mask(creds);
+    if (!((creds_mask & SD_BUS_CREDS_EUID) && (creds_mask & SD_BUS_CREDS_EGID)
+        && (creds_mask & SD_BUS_CREDS_SUPPLEMENTARY_GIDS))) {
+        fprintf(stderr, "uid, gid or supplementary gids of caller not available\n");
+        return -ENODATA;
+    }
+    
+    const char *name, *req;
+    
+    auto r = sd_bus_message_read(m, "as", 2, &name, &req);
+    if (r < 0) {
+        fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
+        return r;
+    }
+    
+    auto& jobs = *(static_cast<jobs_t*>(userdata));
+    return sd_bus_reply_method_return(m, "t", jobs.add(name, req, creds));
+}
+
+extern "C" int jdel(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
+    auto* creds = sd_bus_message_get_creds(m);
+    auto creds_mask = sd_bus_creds_get_mask(creds);
+    if (!((creds_mask & SD_BUS_CREDS_EUID) && (creds_mask & SD_BUS_CREDS_EGID)
+        && (creds_mask & SD_BUS_CREDS_SUPPLEMENTARY_GIDS))) {
+        fprintf(stderr, "uid, gid or supplementary gids of caller not available\n");
+        return -ENODATA;
+    }
+    
+    jid_t *jids;
+    size_t sz;
+    
+    auto r = sd_bus_message_read_array(m, 't', (const void**) &jids, &sz); // the signature is really "at"
+    if (r < 0) {
+        fprintf(stderr, "Failed to parse parameters: %s\n", strerror(-r));
+        return r;
+    }
+    
+    auto& jobs = *(static_cast<jobs_t*>(userdata));
+    return sd_bus_reply_method_return(m, "s", jobs.del(sz/sizeof(jid_t*), jids, creds).c_str());
 }
 
 
 
 extern "C" int jlst(sd_bus_message *m, void *userdata, sd_bus_error *ret_error) {
-        jobs_t& jobs = *(static_cast<jobs_t*>(userdata));
-        
-        std::ostringstream s;
-        s << std::left << '\n' << jobs;
-        return sd_bus_reply_method_return(m, "s", s.str().c_str());
+        auto& jobs = *(static_cast<jobs_t*>(userdata));
+        return sd_bus_reply_method_return(m, "s", jobs.lst().c_str());
 }
 
 // from Lennart's blog
@@ -265,14 +310,13 @@ int main(int argc, char *argv[]) {
         goto finish;
     }
 
-
 //         will not set creds without this.
-    r = sd_bus_negotiate_creds(bus, 1, _SD_BUS_CREDS_ALL);
+//     r = sd_bus_negotiate_creds(bus, 1, _SD_BUS_CREDS_ALL);
+    r = sd_bus_negotiate_creds(bus, 1, SD_BUS_CREDS_EUID|SD_BUS_CREDS_EGID|SD_BUS_CREDS_SUPPLEMENTARY_GIDS);
     if (r < 0) {
         fprintf(stderr, "Failed to negotiate creds: %s\n", strerror(-r));
         goto finish;
     }
-    
     
     r = sd_bus_add_object_vtable(bus,
                                 &slot,
